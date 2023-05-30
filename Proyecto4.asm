ad874a778b55286a4262f2fca724a82f9a57c897
Nancy Mazariegos <99931623+nancygmm@users.noreply.github.com>
2023-05-29 19:21:46 -0600

    Update Proyecto4.asm

--- Proyecto4.asm
+++ Proyecto4.asm
@@ -1,1 +1,8 @@
-
+;Universidad del Valle de Guatemala
+;Organización de computadoras y assembler
+;Profesor Roger Diaz
+;==================> Proyecto 4: par-impar <==================
+;Integrantes:
+;Mauricio Lemus 22461
+;Nancy Mazariegos 22513
+;Santiago Pereira 22318
