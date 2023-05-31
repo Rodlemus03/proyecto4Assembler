bc6af084106a6944c9978948e566515cb99780ec
Nancy Mazariegos <99931623+nancygmm@users.noreply.github.com>
2023-05-30 21:45:08 -0600

    Random

--- main.asm
+++ main.asm
@@ -8,8 +8,21 @@
 ;Santiago Pereira 22318
 ;Monica Salvatierra 22249
 
+.386
+.model flat, stdcall, c
+.stack 4096
+
 .data
+randN BYTE 0
 
 .code
+includelib libucrt.lib
+includelib legacy_stdio_definitions.lib
+includelib libcmt.lib
+includelib libvcruntime.lib
+
+extrn printf:near
+extrn scanf:near
+extrn exit:near
 
 
